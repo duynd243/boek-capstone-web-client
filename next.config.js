08fd34cf0f13d496bbc29c7ff62f6dafa567b940
duyndse <duyndse@gmail.com>
2022-11-21 04:30:46 +0700

    Setup eslint, prettier, tailwind format, ...

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <section>
       <div className="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-          <div className="bg-blue-600 p-8 md:p-12 lg:px-16 lg:py-24">
+          <div className="bg-blue-600 p-8  md:p-12 lg:px-16 lg:py-24">
             <div className="mx-auto max-w-xl text-center">
               <h2 className="text-2xl font-bold text-white md:text-3xl">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit
@@ -27,12 +27,16 @@ export default function Home() {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4 md:grid-cols-1 lg:grid-cols-2">
-            <img
+            <Image
+              width={600}
+              height={400}
               alt="Student"
               src="https://images.unsplash.com/photo-1621274790572-7c32596bc67f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80"
               className="h-40 w-full object-cover sm:h-56 md:h-full"
             />
-            <img
+            <Image
+              width={600}
+              height={400}
               alt="Student"
               src="https://images.unsplash.com/photo-1567168544813-cc03465b4fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
               className="h-40 w-full object-cover sm:h-56 md:h-full"
