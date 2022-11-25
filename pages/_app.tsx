a855ada4065587dfdfbbec26433c9600db128a9a
duyndse <duyndse@gmail.com>
2022-11-25 10:45:16 +0700

    Setup react-query and react-query-devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import "../styles/global.scss";
 import type { AppProps } from "next/app";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
