26da0dd813055e3a4b9ea577a12bffdd35e7a106
MSI\ADMIN <hungtmse150230@fpt.edu.vn>
2023-01-06 00:55:09 +0700

    update

--- components/Modal/BookModal.tsx
+++ components/Modal/BookModal.tsx
@@ -291,9 +291,6 @@ const BookModal: React.FC<Props> = ({ action, maxWidth, isOpen, onClose, book })
                                 className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 disabled:cursor-not-allowed disabled:bg-gray-100 sm:text-sm"
                                 type="text"
                             />
-                            {form.errors.bookName && form.touched.bookName && (
-                                <ErrorMessage>{form.errors.bookName}</ErrorMessage>
-                            )}
                         </div>
                     </div>
                     <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
@@ -330,9 +327,6 @@ const BookModal: React.FC<Props> = ({ action, maxWidth, isOpen, onClose, book })
                                     // className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                     type="text"
                                 />
-                                {form.errors.bookName && form.touched.bookName && (
-                                    <ErrorMessage>{form.errors.bookName}</ErrorMessage>
-                                )}
                             </div>
                         </div>
                         <div className="sm:col-span-3">
